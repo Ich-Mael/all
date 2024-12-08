874cd2d49a65b8648169c6fbdc03756f5521a100
Ich-Mael <ismaelzerbo18@gmail.com>
2024-12-08 22:51:55 +0000

    testimonials redesign

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV !== "production") {
-     require("dotenv").config();
+  require("dotenv").config();
 }
 
 const moment = require("moment");
@@ -13,12 +13,8 @@ const User = require("./models/user");
 const DateUtil = require("./utilities/date");
 const catchAsync = require("./utilities/catchAsync");
 const ExpressError = require("./utilities/ExpressError");
+const { news, allNews } = require("./models/news");
 
-const {
-    news,
-    allNews
-
-} = require("./models/news");
 // Routes requirements
 const userRoutes = require("./routes/user");
 const newsRoutes = require("./routes/news");
@@ -36,25 +32,20 @@ const LocalStrategy = require("passport-local");
 const MongoDBStore = require("connect-mongo");
 
 // bring in the app constants
-const {
-    DB,
-    PORT,
-    SECRET
-} = require("./config");
+const { DB, PORT, SECRET } = require("./config");
 
 // connecting to the database
 const connectDB = catchAsync(async () => {
-    await mongoose.connect(process.env.APP_DB, {
-        dbName: "all",
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
-        useUnifiedTopology: true,
-    });
+  await mongoose.connect(process.env.APP_DB, {
+    dbName: "all",
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  });
 });
 
 connectDB();
-
 
 const app = express();
 // setting up ejs-mate
@@ -65,9 +56,11 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 // parsing the body
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
 
 // setting for method override
 app.use(methodOverride("_method"));
@@ -77,18 +70,18 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // setting up session
 const sessionConfig = {
-    store: MongoDBStore.create({
-        mongoUrl: process.env.APP_DB,
-        touchAfter: 24 * 60 * 60,
-    }),
-    secret: process.env.SECRET || "hereistheworstsecretever",
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        httponly: true,
-        expires: Date.now() + 1000 * 60 * 60 * 12 * 1,
-        maxAge: 1000 * 60 * 60 * 24 * 7,
-    },
+  store: MongoDBStore.create({
+    mongoUrl: process.env.APP_DB,
+    touchAfter: 24 * 60 * 60,
+  }),
+  secret: process.env.SECRET || "hereistheworstsecretever",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httponly: true,
+    expires: Date.now() + 1000 * 60 * 60 * 12 * 1,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
 };
 
 //for cookies
@@ -102,56 +95,62 @@ app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(
-    User.serializeUser(function (user, done) {
-        done(null, user);
-    })
+  User.serializeUser(function (user, done) {
+    done(null, user);
+  }),
 );
 passport.deserializeUser(
-    User.deserializeUser(function (user, done) {
-        done(null, user);
-    })
+  User.deserializeUser(function (user, done) {
+    done(null, user);
+  }),
 );
 
 // setting up local variables
 app.use((req, res, next) => {
-    res.locals.currentTime = DateUtil.localTime(Date.now(), "Date du Jour :");
-    res.locals.currentTimeEng = moment().format("dddd, Do MMMM  YYYY");
-    res.locals.currentUser = req.user;
-    res.locals.success = req.flash("success");
-    res.locals.error = req.flash("error");
-    next();
+  res.locals.currentTime = DateUtil.localTime(Date.now(), "Date du Jour :");
+  res.locals.currentTimeEng = moment().format("dddd, Do MMMM  YYYY");
+  res.locals.currentUser = req.user;
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
 });
 
 // Landing page route
-app.get("/e444", catchAsync(async (req, res) => {
-    
-    res.render("afrohk_home", {
-    });
-}));
+app.get(
+  "/e444",
+  catchAsync(async (req, res) => {
+    res.render("afrohk_home", {});
+  }),
+);
 
 // Language Lab  home page
-app.get("/", catchAsync(async (req, res) => {
-
-    res.render("all_home")
-}));
-
+app.get(
+  "/",
+  catchAsync(async (req, res) => {
+    res.render("all_home");
+  }),
+);
 
 // Language Lab  home page
-app.get("/elearning", catchAsync(async (req, res) => {
+app.get(
+  "/elearning",
+  catchAsync(async (req, res) => {
     res.render("comingsoon");
-}));
-
+  }),
+);
 
 // Language Lab  home page
-app.get("/tech-innovation", catchAsync(async (req, res) => {
+app.get(
+  "/tech-innovation",
+  catchAsync(async (req, res) => {
     res.render("comingsoon");
-}));
-
+  }),
+);
 
 // Landing page route
 app.get("/date", (req, res) => {
-    console.log(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
-    res.send(" Date test");
+  console.log(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+  res.send(" Date test");
 });
 
 //user routes
@@ -160,20 +159,17 @@ app.use("/", userRoutes);
 //courses routes
 app.use("/", courseRoutes);
 
-
 //comments routes
 app.use("/", commentRoutes);
 
 //comments routes
 app.use("/", newsRoutes);
 
-
 // english for everybody routes
 app.use("/", englishRoutes);
 
 // english for everybody routes
 app.use("/", pecpRoutes);
-
 
 // English for evryone
 app.use("/", english4EveroneRoutes);
@@ -185,24 +181,22 @@ app.use("/", grammarCP);
 app.use("/", english4ProfessionRoutes);
 
 app.all("*", (req, res, next) => {
-    next(new ExpressError("Page not found", 404));
+  next(new ExpressError("Page not found", 404));
 });
 
 //error handling
 
 app.use((err, req, res, next) => {
-    const {
-        statusCode = 500
-    } = err;
-    if (!err.message)
-        err.message = `Quelque chose d'inattendue s'est produit 🤷🏽‍♂️!!!`;
-    res.status(statusCode).render("error", {
-        err
-    });
+  const { statusCode = 500 } = err;
+  if (!err.message)
+    err.message = `Quelque chose d'inattendue s'est produit 🤷🏽‍♂️!!!`;
+  res.status(statusCode).render("error", {
+    err,
+  });
 });
 
 const port = process.env.PORT || 8000;
 // server
 app.listen(port, () => {
-    console.log("server is working");
+  console.log("server is working");
 });
--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -178,72 +178,48 @@
   /**
    * Testimonials slider
    */
-  new Swiper('.testimonials-slider', {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
+
+  
+  /**
+   * Init swiper sliders
+   */
+  function initSwiper() {
+    document.querySelectorAll(".init-swiper").forEach(function(swiperElement) {
+      let config = JSON.parse(
+        swiperElement.querySelector(".swiper-config").innerHTML.trim()
+      );
+
+      if (swiperElement.classList.contains("swiper-tab")) {
+        initSwiperWithCustomPagination(swiperElement, config);
+      } else {
+        new Swiper(swiperElement, config);
+      }
+    });
+  }
+
+  window.addEventListener("load", initSwiper);
+
+  /**
+   * Correct scrolling position upon page load for URLs containing hash links.
+   */
+  window.addEventListener('load', function(e) {
+    if (window.location.hash) {
+      if (document.querySelector(window.location.hash)) {
+        setTimeout(() => {
+          let section = document.querySelector(window.location.hash);
+          let scrollMarginTop = getComputedStyle(section).scrollMarginTop;
+          window.scrollTo({
+            top: section.offsetTop - parseInt(scrollMarginTop),
+            behavior: 'smooth'
+          });
+        }, 100);
+      }
     }
   });
 
-  /**
-   * Porfolio isotope and filter
-   */
-  window.addEventListener('load', () => {
-    let portfolioContainer = select('.portfolio-container');
-    if (portfolioContainer) {
-      let portfolioIsotope = new Isotope(portfolioContainer, {
-        itemSelector: '.portfolio-item'
-      });
 
-      let portfolioFilters = select('#portfolio-flters li', true);
 
-      on('click', '#portfolio-flters li', function(e) {
-        e.preventDefault();
-        portfolioFilters.forEach(function(el) {
-          el.classList.remove('filter-active');
-        });
-        this.classList.add('filter-active');
-
-        portfolioIsotope.arrange({
-          filter: this.getAttribute('data-filter')
-        });
-
-      }, true);
-    }
-
-  });
-
-  /**
-   * Initiate portfolio lightbox 
-   */
-  const portfolioLightbox = GLightbox({
-    selector: '.portfolio-lightbox'
-  });
-
-  /**
-   * Portfolio details slider
-   */
-  new Swiper('.portfolio-details-slider', {
-    speed: 400,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    }
-  });
+  
 
   /**
    * Initiate Pure Counter 
